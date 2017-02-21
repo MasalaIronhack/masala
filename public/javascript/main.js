293d545565cd1762f68ef958fa686b1b84c8c007
ManuLintz <manuel.lintz@gmail.com>
2017-02-21 14:20:11 +0100

    after lunch

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,3 +1,15 @@
+//require('dotenv').config();
+function showFeedback (postResponse) {
+  console.log('post success');
+  console.log(postResponse);
+}
+
+function handleError (err) {
+  console.log('Oh no! Error:');
+  console.log(err);
+}
+
+
 FB.init({
   appId      : '864439233697725',
   status     : true,
@@ -6,57 +18,33 @@ FB.init({
 });
 
 
-function saveTo(data){
-$.ajax({
-      url: "'mongodb://localhost/masala/users",
-      method: "POST",
-      data:    data,
-      success: (response) =>{
-        console.log('success!');
-        console.log(response);
-      },
-      error:   handleError
-    })
-}
-
-
-
 function getFBData () {
-  FB.api(
-  '/me',
-  'GET',
+  FB.api('/me',
+         'GET',
+        {"fields":"music,books,likes,events,movies,television,games,friendlists,taggable_friends",
+         "access_token" :"EAAMSMZCF0V70BAEb1uJARIzewWqPY20l6WzZAb8fLqshbz1bwl3QsJdNzFqItPh2JsXn5FpERx1s7abvxZCpPka0IHhpXlwPAeaXZBRN0n6ZAA2gZAin3ulOVHz4ZATM1NRWkmNjehosrV74foGiRalhjsZA3dMhQRfRZB3k9aaU8xMMeL63oPryN"
+        },
+            function(response) {
+              console.log(response);
+              let obj = {hola: "hola"};
 
-  {"fields":"music,books,likes,events,movies,television,games,friendlists,taggable_friends",
-    "access_token" :"EAAMSMZCF0V70BACEZARvzPsgudbyC6SbDSAEJesRnJtOzni4fBw4ZBLH8plJOrIF6f7TEY7ZBblNPqC72W3UrVLUKE0BtkwTQZC67b6rPmNZBAZAvs0yzZBU6oh1idB8eoYwB2RhgIOokuNNeuOWoXh5FeaiEiEW6YkjrlivMB0ByvFt3IZAlZCO1G"},
-  function(response) {
-    console.log(response);
-    var datas = response;
-    return datas;
-
-    }
-  );
-}
-
-
-$
-
-
-function showFeedback (postResponse) {
-console.log('post success');
-console.log(postResponse);
-}
-
-function handleError (err) {
-console.log('Oh no! Error:');
-console.log(err);
-}
+              $.ajax({
+                  type: "POST",
+                  data: JSON.stringify(obj),
+                  contentType: "application/json",
+                  url:     '/account',
+                  success: showFeedback,
+                  error:   handleError
+                });
+            });    }
 
 
 
 $(document).ready(function() {
+  $('button').on('click', function(){
+    console.log(datas);
+  })
 getFBData();
-
-
 
 
 });
--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var express = require('express');
 var router = express.Router();
 const passport = require('passport');
@@ -15,7 +16,7 @@ router.get(
 router.get('/auth/facebook/callback',
   passport.authenticate('facebook', { session: false, failureRedirect: "/" }),
   function(req, res) {
-    res.redirect("/account?access_token=" +"EAAMSMZCF0V70BACEZARvzPsgudbyC6SbDSAEJesRnJtOzni4fBw4ZBLH8plJOrIF6f7TEY7ZBblNPqC72W3UrVLUKE0BtkwTQZC67b6rPmNZBAZAvs0yzZBU6oh1idB8eoYwB2RhgIOokuNNeuOWoXh5FeaiEiEW6YkjrlivMB0ByvFt3IZAlZCO1G");
+    res.redirect("/account?access_token=" +process.env.FB_TOKEN);
     // req.user.access_token);
   }
 );
