f8184e2c4ec4c995de8322877d2d63930158dc5b
juliapolbach <juliapolbach@gmail.com>
2017-02-21 14:38:37 +0100

    css

--- app.js
+++ app.js
@@ -86,11 +86,6 @@ console.log(events);
 getMeetUpEvents();
 ////////////////////////
 
-app.use('/', index);
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -98,6 +93,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // catch 404 and forward to error handler
 /*app.use(function(req, res, next) {
